9c3f29223eee33d148132588ced5621c98f781e8
Lokesh <lokeshsingh7695@gmail.com>
2024-01-08 11:46:57 +0530

    Basic hello world with command how to run java by cmd

---
+++ src/firstProgram.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+public class firstProgram{
+    public static void main(String [] args){
+        System.out.print("Hello World");
+    }
+}
+
+// For compiling the program
+// javac firstProgram.java
+
+// For running the program
+// java firstProgram
+
+// Output
+// Hello World
