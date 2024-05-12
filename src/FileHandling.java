04afee91db9da5b5a04432629f73a1704938de5a
Lokesh <lokeshsingh7695@gmail.com>
2024-05-12 19:09:53 +0530

    File Handling in Java

--- src/FileHandling.java
+++ src/FileHandling.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class FileHandling {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+public class FileHandling {
+    public static void main(String[] args) {
+
+        // Creation of file
+
+    File myFile = new File("lokesh.txt");
+    try {
+        myFile.createNewFile();
+    } catch (Exception e) {
+        System.out.println("Some Error Occured During Creation of file\n");
+    }
+
+
+    // Writing in file
+    try{
+    FileWriter fileWriter = new FileWriter("lokesh.txt");
+        fileWriter.write("This is is File Handling\nI am Lokesh singh\nI am going to finish It");
+        fileWriter.close();
+    } catch (IOException e) {
+        System.out.println("Unable to write a file");
+        e.printStackTrace();
+    }
+
+    // Reading a file
+    File file = new File("lokesh.txt");
+    try (Scanner sc = new Scanner(file)) {
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+    } catch (FileNotFoundException e) {
+        System.out.println("Some Error Occured During Reading the file");
+        e.printStackTrace();
+    }
+
+
+        // Delete the file
+
+        File f = new File("lokesh.txt");
+        f.delete();
+
+    }
 }
